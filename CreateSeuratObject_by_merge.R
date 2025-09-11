419b3493a01fcde006992296d0ff2449843e7162
ra1n-zjy <zhuan9.j.y@gmail.com>
2025-09-11 11:28:24 +0800

    Add files via upload

---
+++ CreateSeuratObject_by_merge.R
@@ -0,0 +1,33 @@
+PCSC_X <- readRDS("/home/data/X/1.data_processing/PCSC_preQC.Rds")
+table(PCSC_X$orig.ident)
+
+PCSC_Y <- readRDS("/home/data/Y/1.data_processing/PCSC_preQC.Rds")
+table(PCSC_Y$orig.ident)
+
+PCSC_preQC <- merge(x = PCSC_X, y = PCSC_Y, merge.data = TRUE)
+table(PCSC_preQC$orig.ident)
+
+PCSC_preQC <- JoinLayers(PCSC_preQC)
+
+folder <- "1.data_processing"
+if(!dir.exists(folder)){
+  dir.create(folder)
+}
+
+PCSC_preQC$Group <- recode(PCSC_preQC$orig.ident,
+                           "" = "",
+                           "" = "",
+                           "" = "",
+                           "" = "",
+                           "" = ""
+)
+
+df <- data.frame(Sample = names(table(PCSC_preQC$orig.ident)), 
+                 preQC_Count = as.numeric(table(PCSC_preQC$orig.ident)))
+
+library(openxlsx)
+write.xlsx(df, "./1.data_processing/cell_counts.xlsx")
+getSheetNames("./1.data_processing/cell_counts.xlsx")
+df <- read.xlsx("./1.data_processing/cell_counts.xlsx", sheet = "Sheet 1")
+
+saveRDS(PCSC,"./1.data_processing/PCSC_preQC.Rds")
